3704d446ac43d38e93185f665cf2a1f4a837971f
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-09-06 00:13:10 -0300

    rebus service provider

--- RebusNetCore/RebusNetCore/Startup.cs
+++ RebusNetCore/RebusNetCore/Startup.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Rebus.Messages;
+using Rebus.Routing.TypeBased;
+using Rebus.ServiceProvider;
+using Rebus.Transport.InMem;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +27,30 @@ namespace RebusNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configure and register Rebus
+
+            var nomeFila = "fila_rebus";
+
+            services.AddRebus(configure => configure
+               .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), nomeFila))
+               //.Transport(t => t.UseRabbitMq("amqp://localhost", nomeFila))
+               .Subscriptions(s => s.StoreInMemory())
+               .Routing(r =>
+               {
+                   r.TypeBased()
+                       .MapAssemblyOf<Message>(nomeFila)
+                       .MapAssemblyOf<RealizarPedidoCommand>(nomeFila)
+                       .MapAssemblyOf<RealizarPagamentoCommand>(nomeFila);
+               })
+               .Sagas(s => s.StoreInMemory())
+               .Options(o =>
+               {
+                   o.SetNumberOfWorkers(1);
+                   o.SetMaxParallelism(1);
+                   o.SetBusName("Demo Rebus");
+               })
+           );
+
             services.AddControllersWithViews();
         }
 
