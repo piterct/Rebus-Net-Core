e9ceb7a597e3820c12553b954f9efa180e10a626
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-09-05 21:57:48 -0300

    Message Core

---
+++ RebusNetCore/Core/Messages/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Core.Messages
+{
+    public  class Message
+    {
+        public Guid AggregateRoot { get; set; }
+        public DateTime DateTime { get; set; }
+        protected Message()
+        {
+            DateTime = DateTime.Now;
+        }
+    }
+}
